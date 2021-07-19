4818c121df4ff17a58e86029da1ff1d2bb6bb994
m-star18 <31807@toyota.kosen-ac.jp>
2021-07-19 14:43:15 +0900

    Add PureMCTS

--- src/mcts.py
+++ src/mcts.py
@@ -221,3 +221,68 @@ class PureTreeNode(object):
 
     def is_root(self):
         return self._parent is None
+
+
+class PureMCTS(object):
+    """
+    A simple implementation of Monte Carlo Tree Search.
+    """
+
+    def __init__(self, policy_value_fn, c_puct=5, n_playout=10000):
+        self._root = TreeNode(None, 1.0)
+        self._policy = policy_value_fn
+        self._c_puct = c_puct
+        self._n_playout = n_playout
+
+    def _playout(self, state):
+        node = self._root
+        while 1:
+            if node.is_leaf():
+
+                break
+            # Greedily select next move.
+            action, node = node.select(self._c_puct)
+            state.do_move(action)
+
+        action_probs, _ = self._policy(state)
+        # Check for end of game
+        end, winner = state.game_end()
+        if not end:
+            node.expand(action_probs)
+        # Evaluate the leaf node by random rollout
+        leaf_value = self._evaluate_rollout(state)
+        # Update value and visit count of nodes in this traversal.
+        node.update_recursive(-leaf_value)
+
+    def _evaluate_rollout(self, state, limit=1000):
+        player = state.get_current_player()
+        for i in range(limit):
+            end, winner = state.game_end()
+            if end:
+                break
+            action_probs = rollout_policy_fn(state)
+            max_action = max(action_probs, key=itemgetter(1))[0]
+            state.do_move(max_action)
+        else:
+            # If no break from the loop, issue a warning.
+            print("WARNING: rollout reached move limit")
+        if winner == -1:  # tie
+            return 0
+        else:
+            return 1 if winner == player else -1
+
+    def get_move(self, state):
+        for n in range(self._n_playout):
+            state_copy = copy.deepcopy(state)
+            self._playout(state_copy)
+        return max(self._root._children.items(), key=lambda act_node: act_node[1]._n_visits)[0]
+
+    def update_with_move(self, last_move):
+        if last_move in self._root._children:
+            self._root = self._root._children[last_move]
+            self._root._parent = None
+        else:
+            self._root = TreeNode(None, 1.0)
+
+    def __str__(self):
+        return "MCTS"
