7af2c4b02f80e8cc383f6d29309fa65fda7e2fae
m-star18 <31807@toyota.kosen-ac.jp>
2021-07-19 14:42:14 +0900

    Add PureTreeNode

--- src/mcts.py
+++ src/mcts.py
@@ -177,3 +177,47 @@ def policy_value_fn(board):
     # return uniform probabilities and 0 score for pure MCTS
     action_probs = np.ones(len(board.availables))/len(board.availables)
     return zip(board.availables, action_probs), 0
+
+
+class PureTreeNode(object):
+    """
+    A node in the MCTS tree.
+    """
+
+    def __init__(self, parent, prior_p):
+        self._parent = parent
+        self._children = {}  # a map from action to TreeNode
+        self._n_visits = 0
+        self._Q = 0
+        self._u = 0
+        self._P = prior_p
+
+    def expand(self, action_priors):
+        for action, prob in action_priors:
+            if action not in self._children:
+                self._children[action] = TreeNode(self, prob)
+
+    def select(self, c_puct):
+        return max(self._children.items(), key=lambda act_node: act_node[1].get_value(c_puct))
+
+    def update(self, leaf_value):
+        # Count visit.
+        self._n_visits += 1
+        # Update Q, a running average of values for all visits.
+        self._Q += 1.0*(leaf_value - self._Q) / self._n_visits
+
+    def update_recursive(self, leaf_value):
+        # If it is not root, this node's parent should be updated first.
+        if self._parent:
+            self._parent.update_recursive(-leaf_value)
+        self.update(leaf_value)
+
+    def get_value(self, c_puct):
+        self._u = (c_puct * self._P * np.sqrt(self._parent._n_visits) / (1 + self._n_visits))
+        return self._Q + self._u
+
+    def is_leaf(self):
+        return self._children == {}
+
+    def is_root(self):
+        return self._parent is None
