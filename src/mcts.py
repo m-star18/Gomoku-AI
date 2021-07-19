12ef698c5fbf54e342e4dab7d36b59bda3c9245e
m-star18 <31807@toyota.kosen-ac.jp>
2021-07-19 14:43:51 +0900

    Add PureMCTSPlayer

--- src/mcts.py
+++ src/mcts.py
@@ -286,3 +286,30 @@ class PureMCTS(object):
 
     def __str__(self):
         return "MCTS"
+
+
+class PureMCTSPlayer(object):
+    """
+    AI player based on MCTS
+    """
+
+    def __init__(self, c_puct=5, n_playout=2000):
+        self.mcts = MCTS(policy_value_fn, c_puct, n_playout)
+
+    def set_player_ind(self, p):
+        self.player = p
+
+    def reset_player(self):
+        self.mcts.update_with_move(-1)
+
+    def get_action(self, board):
+        sensible_moves = board.availables
+        if len(sensible_moves) > 0:
+            move = self.mcts.get_move(board)
+            self.mcts.update_with_move(-1)
+            return move
+        else:
+            print("WARNING: the board is full")
+
+    def __str__(self):
+        return "MCTS {}".format(self.player)
