0a236188ef8718001aacfe5431889e045f56db8e
m-star18 <31807@toyota.kosen-ac.jp>
2021-07-19 14:07:01 +0900

    Add init_board

--- src/env/game.py
+++ src/env/game.py
@@ -23,3 +23,12 @@ class Board(object):
         # need how many pieces in a row to win
         self.n_in_row = int(kwargs.get('n_in_row', WIN))
         self.players = [1, 2]  # player1 and player2
+
+    def init_board(self, start_player=0):
+        if self.width < self.n_in_row or self.height < self.n_in_row:
+            raise Exception(f'board width and height can not be less than {self.n_in_row}')
+        self.current_player = self.players[start_player]
+        # keep available moves in a list
+        self.availables = list(range(self.width * self.height))
+        self.states = {}
+        self.last_move = -1
