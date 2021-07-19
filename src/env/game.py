69e7efa60234402aaf53cf9f9569a848c1bb9619
m-star18 <31807@toyota.kosen-ac.jp>
2021-07-19 14:14:31 +0900

    Add start_play

--- src/env/game.py
+++ src/env/game.py
@@ -150,3 +150,32 @@ class Game(object):
                 else:
                     print('_'.center(HEIGHT), end='')
             print('\r\n\r\n')
+
+    def start_play(self, player1, player2, start_player=0, is_shown=1):
+        if start_player not in (0, 1):
+            raise Exception('start_player should be either 0 (player1 first) or 1 (player2 first)')
+
+        self.board.init_board(start_player)
+        p1, p2 = self.board.players
+        player1.set_player_ind(p1)
+        player2.set_player_ind(p2)
+        players = {p1: player1, p2: player2}
+        if is_shown:
+            self.graphic(self.board, player1.player, player2.player)
+        while True:
+            current_player = self.board.get_current_player()
+            player_in_turn = players[current_player]
+            move = player_in_turn.get_action(self.board)
+            self.board.do_move(move)
+            if is_shown:
+                self.graphic(self.board, player1.player, player2.player)
+            end, winner = self.board.game_end()
+            if end:
+                if is_shown:
+                    if winner != -1:
+                        print("Game end. Winner is", players[winner])
+                    else:
+                        print("Game end. Tie")
+
+                return winner
+
