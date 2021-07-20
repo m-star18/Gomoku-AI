05bec9c7c8db78fdf3652e4bc2ad710889b99f6e
m-star18 <31807@toyota.kosen-ac.jp>
2021-07-20 09:08:31 +0900

    Add TrainPipeline

--- src/train.py
+++ src/train.py
@@ -6,3 +6,52 @@ from src.env.game import Board, Game
 from src.mcts import PureMCTSPlayer as MCTS_Pure
 from src.mcts import MCTSPlayer
 from src.policy_value_net import PolicyValueNet  # Pytorch
+from src.const import (
+    HEIGHT,
+    WIDTH,
+    WIN,
+)
+
+
+class TrainPipeline:
+    def __init__(self, init_model=None):
+        # params of the board and the game
+        self.board_width = WIDTH
+        self.board_height = HEIGHT
+        self.n_in_row = WIN
+        self.board = Board(width=self.board_width,
+                           height=self.board_height,
+                           n_in_row=self.n_in_row)
+        self.game = Game(self.board)
+        # training params
+        self.learn_rate = 2e-3
+        self.lr_multiplier = 1.0  # adaptively adjust the learning rate based on KL
+        self.temp = 1.0  # the temperature param
+        self.n_playout = 400  # num of simulations for each move
+        self.c_puct = 5
+        self.buffer_size = 10000
+        self.batch_size = 512  # mini-batch size for training
+        self.data_buffer = deque(maxlen=self.buffer_size)
+        self.play_batch_size = 1
+        self.epochs = 5  # num of train_steps for each update
+        self.kl_targ = 0.02
+        self.check_freq = 50
+        self.game_batch_num = 1500
+        self.best_win_ratio = 0.0
+        # num of simulations used for the pure mcts, which is used as
+        # the opponent to evaluate the trained policy
+        self.pure_mcts_playout_num = 1000
+        if init_model:
+            # start training from an initial policy-value net
+            self.policy_value_net = PolicyValueNet(self.board_width,
+                                                   self.board_height,
+                                                   model_file=init_model)
+        else:
+            # start training from a new policy-value net
+            self.policy_value_net = PolicyValueNet(self.board_width,
+                                                   self.board_height)
+        self.mcts_player = MCTSPlayer(self.policy_value_net.policy_value_fn,
+                                      c_puct=self.c_puct,
+                                      n_playout=self.n_playout,
+                                      is_selfplay=1)
+
